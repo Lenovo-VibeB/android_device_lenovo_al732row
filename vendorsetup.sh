3be2ec75c0911ee2c5aaa6a7c37fa334a6a25c2e
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-05-16 19:09:24 -0500

    minnor fix

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,4 @@
-# Use this to add x5pro to CM's lunch command menu
-add_lunch_combo lineage_al732row-userdebug
+# Use this to add al732row to CM's lunch command menu
+for var in eng user userdebug; do
+  add_lunch_combo lineage_al732row-$var
+done
