7c82aa36c751b392e7a6eda5c393c042f1ff8804
CarlosArriagaCM <CarlosArriagaCM@gmail.com>
2017-06-01 18:33:24 -0500

    al732row: cleanup device tree

--- device_al732row.mk
+++ device_al732row.mk
@@ -144,9 +144,6 @@ PRODUCT_PACKAGES += \
 $(shell mkdir -p $(OUT)/obj/KERNEL_OBJ/usr)
 $(shell touch $(OUT)/obj/KERNEL_OBJ/usr/export_includes)
 
-##
-#$(call inherit-product, build/target/product/full.mk)
-
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_al732row
 PRODUCT_DEVICE := al732row
