fb4467097755dfac1e39b91490b9ad172a7cc307
Carlos Arriaga <carlosarriagacm@gmail.com>
2017-08-04 13:56:39 -0500

    al732row: set in prop

--- device_al732row.mk
+++ device_al732row.mk
@@ -22,10 +22,6 @@ DEVICE_PACKAGE_OVERLAYS += device/lenovo/al732row/overlay
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Screen density
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=300
-
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 	
